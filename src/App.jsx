192dfc18480bbf5a7ef0380adbdbee22c9c80082
sahilsawantGEC <sahilsawant004008@gmail.com>
2024-09-04 13:47:17 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 
 function App() {
+  const [loading, setloading] = useState(true);
+  const dispatch = useDispatch();
 
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setloading(false));
+  }, []);
 
-  return (
-    <>
-     <h1>Welcome to my Blog App with appwrite</h1>
-    </>
-  )
+  return !loading?(
+    <div>
+      
+    </div>
+  ):null
 }
 
-export default App
+export default App;
