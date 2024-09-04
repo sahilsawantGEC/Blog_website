57c98a6ea189d015c08dc4b10afb8b7921ce04a2
sahilsawantGEC <sahilsawant004008@gmail.com>
2024-09-04 12:10:46 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+     <h1>Welcome to my Blog App with appwrite</h1>
+    </>
+  )
+}
+
+export default App
