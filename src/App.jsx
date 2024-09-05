b1bd36aa91a036486761a692db720a8fd6ea0751
sahilsawantGEC <sahilsawant004008@gmail.com>
2024-09-05 22:30:40 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import { useDispatch } from "react-redux";
 import authService from "./appwrite/auth";
 import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components";
+import {Outlet} from "react-router-dom";
 
 function App() {
   const [loading, setloading] = useState(true);
@@ -21,11 +23,17 @@ function App() {
       .finally(() => setloading(false));
   }, []);
 
-  return !loading?(
-    <div>
-      
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main>
+        <Footer />
+      </div>
     </div>
-  ):null
+  ) : null;
 }
 
 export default App;
